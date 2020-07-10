1b4456f8a81f054037725bb5191394ba32d5405f
RainASdM-PC <rainasdmdhr@gmail.com>
2020-07-10 22:36:15 +0800

    加入typescript代码

---
+++ react_app/src/public_Components/Nav/lib/functions.tsx
@@ -0,0 +1,85 @@
+/**
+ *  @author: RainASdM
+ *  @date: 2020-05-22 14:12
+ *  @description  组件引用方法集
+ */
+
+//引入依赖库
+import Request from "../../../utils/Request";
+
+//接口类型
+interface logIn_interface {
+    useName: string,
+    password: string
+}
+
+interface logIn_Callback_interface {
+
+}
+
+interface singOut_interface {
+    signOut_Callback : () => {};
+}
+
+interface signOut_Callback_interface {
+    result: object;
+}
+
+/**
+ * 获取登录当前状态
+ */
+const getLoginStatus : () => {} = () => {
+
+
+    return {
+        status: false,
+        value: {}
+    };
+}
+
+/**
+ *
+ */
+const logIn = ({useName, password} : logIn_interface) => {
+    Request.POST({
+        params: {useName, password},
+        path: "/login",
+        callback: (result: object) => {
+            logIn_Callback(result)
+        }
+    });
+}
+
+/**
+ * 登录回调方法
+ * @param result
+ */
+const logIn_Callback = (result : logIn_Callback_interface) => {
+
+}
+
+
+const singOut = (signOut_Callback: singOut_interface) => {
+    Request.GET({
+        path: "/signOut",
+        callback: (result: object) => {
+            signOut_Callback(result);
+        }
+    })
+}
+
+/**
+ * 退出登录回调方法
+ * @param result
+ */
+const signOut_Callback = (result : signOut_Callback_interface) => {
+
+}
+
+export {
+    getLoginStatus,
+    logIn,
+    logIn_Callback,
+    singOut,
+    signOut_Callback
+}
